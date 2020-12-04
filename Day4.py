8a4039751346c583807378c67e3ba29fe8dd66bd
Rylande <frankmennink@gmail.com>
2020-12-04 10:17:48 +0100

    Added length constraint for hcl

--- Day4.py
+++ Day4.py
@@ -27,7 +27,7 @@ def validatePassport(passport):
     if int(passport["byr"]) < 1920 or int(passport["byr"]) > 2002: return False
     if int(passport["iyr"]) < 2010 or int(passport["iyr"]) > 2020: return False
     if int(passport["eyr"]) < 2020 or int(passport["eyr"]) > 2030: return False
-    if re.search("#[0-9a-f]{6}", passport["hcl"]) == None: return False
+    if re.search("#[0-9a-f]{6}", passport["hcl"]) == None or len(passport["hcl"]) != 7: return False
     if passport["ecl"] not in ["amb","blu","brn","gry","grn","hzl","oth"]: return False
     if re.search("[0-9]{9}", passport["pid"]) == None or len(passport["pid"]) != 9: return False
     if passport["hgt"][-2:] == "cm":
