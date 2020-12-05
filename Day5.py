ea432adc0d247c67dc484575f22c7923ba7990ab
Rylande <frankmennink@gmail.com>
2020-12-05 10:24:32 +0100

    Day5

---
+++ Day5.py
@@ -0,0 +1,15 @@
+inputString = open("Day5Input.txt", "r").read().splitlines()
+seatList = []
+
+def readSeats():
+    for scan in inputString:
+        seatList.append(int(scan.replace("F", "0").replace("B", "1").replace("R", "1").replace("L", "0"), 2))
+    seatList.sort(reverse=True)
+
+def findSeat():
+    for seat in seatList:
+        if seat - 2 in seatList and seat - 1 not in seatList: return seat - 1
+
+readSeats()
+print(seatList[0])
+print(findSeat())
