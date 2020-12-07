0c994aabfb5adb73ba4307b200e770571acfdf0f
Rylande <frankmennink@gmail.com>
2020-12-07 09:11:36 +0100

    Added set implementation for Day 6

--- Day6.py
+++ Day6.py
@@ -19,4 +19,26 @@ def readAnswers():
     print(totalCount)
     print(totalP2Count)
 
+def readAnswersSet():
+    totalP1Count, totalP2Count, setP1, setP2, newEntry = 0, 0, set(), set(), True
+    for line in inputString:
+        if line == "":
+            totalP1Count += len(setP1)
+            totalP2Count += len(setP2)
+            newEntry = True
+        else:
+            if newEntry:
+                setP1 = set(line)
+                setP2 = set(line)
+                newEntry = False
+            else:
+                setP1.update(set(line))
+                setP2.intersection_update(set(line))
+    totalP1Count += len(setP1)
+    totalP2Count += len(setP2)
+    print(totalP1Count)
+    print(totalP2Count)
+
+
 readAnswers()
+readAnswersSet()
