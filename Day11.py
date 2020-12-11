440da14f7705ec21ce805b792bbedff8e448382d
Rylande <frankmennink@gmail.com>
2020-12-11 11:46:07 +0100

    Day 11 optimizations

--- Day11.py
+++ Day11.py
@@ -1,4 +1,4 @@
-import copy
+from copy import deepcopy
 
 inputString = open("Day11Input.txt", "r").read().splitlines()
 
@@ -14,56 +14,17 @@ def countSeats(x, y, data, countVisible):
         count = sum(line[x-1:x+2].count('#') for line in data[y-1:y+2])
         if data[y][x] == '#': count -= 1
     else:
-        count, currX, currY = 0, x, y
-        # L
-        currX -= 1
-        while data[currY][currX] == '.' and currX > 0:
-            currX -= 1
-        if data[currY][currX] == "#": count += 1
-        # R
-        currX = x + 1
-        while data[currY][currX] == '.' and currX < len(data[0]) - 1:
-            currX += 1
-        if data[currY][currX] == "#": count += 1
-        # U
-        currX, currY = x, y - 1
-        while data[currY][currX] == '.' and currY > 0:
-            currY -= 1
-        if data[currY][currX] == "#": count += 1
-        # D
-        currX, currY = x, y + 1
-        while data[currY][currX] == '.' and currY < len(data) - 1:
-            currY += 1
-        if data[currY][currX] == "#": count += 1
-        # LU
-        currX, currY = x - 1, y - 1
-        while data[currY][currX] == '.' and currX > 0 and currY > 0:
-            currX -= 1
-            currY -= 1
-        if data[currY][currX] == "#": count += 1
-        # LD
-        currX, currY = x - 1, y + 1
-        while data[currY][currX] == '.' and currX > 0 and currY < len(data) - 1:
-            currX -= 1
-            currY += 1
-        if data[currY][currX] == "#": count += 1
-        # RD
-        currX, currY = x + 1, y + 1
-        while data[currY][currX] == '.' and currX < len(data[0]) - 1 and currY < len(data) - 1:
-            currX += 1
-            currY += 1
-        if data[currY][currX] == "#": count += 1
-        # RU
-        currX, currY = x + 1, y - 1
-        while data[currY][currX] == '.' and currX < len(data[0]) - 1 and currY > 0:
-            currX += 1
-            currY -= 1
-        if data[currY][currX] == "#": count += 1
+        count, operations = 0, [[-1, 0], [1, 0], [0, 1], [0, -1], [-1, -1], [-1, 1], [1, -1], [1, 1]]
+        for op in operations:
+            currX, currY = x + op[0], y + op[1]
+            while data[currY][currX] == '.' and 0 < currX < len(data[0]) - 1 and 0 < currY < len(data) - 1:
+                currX, currY = currX + op[0], currY + op[1]
+            if data[currY][currX] == "#": count += 1
     return count
 
 def updateState(data, threshold, countVisible):
     stable = True
-    newData = copy.deepcopy(data)
+    newData = deepcopy(data)
     for x in range(1, len(data[0])):
         for y in range(1, len(data)):
             if data[y][x] == 'L':
