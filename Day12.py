99b111cbe1cbe768ffc0269786bf605aa24cbed7
Rylande <frankmennink@gmail.com>
2020-12-12 11:37:08 +0100

    Day 12

--- Day12.py
+++ Day12.py
@@ -7,9 +7,9 @@ def partOne():
     x, y, currDirIdx = 0, 0, 1
     for line in inputString:
         if line[:1] == "L":
-            currDirIdx = (currDirIdx - int(int(line[1:])/90)) % 4
+            currDirIdx = (currDirIdx - int(int(line[1:]) / 90)) % 4
         elif line[:1] == "R":
-            currDirIdx = (currDirIdx + int(int(line[1:])/90)) % 4
+            currDirIdx = (currDirIdx + int(int(line[1:]) / 90)) % 4
         elif line[:1] == "F":
             x += translations[directions[currDirIdx]][0] * int(line[1:])
             y += translations[directions[currDirIdx]][1] * int(line[1:])
@@ -27,7 +27,7 @@ def partTwo():
                 newY = wayX
                 wayX, wayY = newX, newY
         elif line[:1] == "R":
-            for i in range(0, int(int(line[1:])/90)):
+            for i in range(0, int(int(line[1:]) / 90)):
                 newX = wayY
                 newY = -wayX
                 wayX, wayY = newX, newY
