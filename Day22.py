8355e62ec356befb5dfffe1c313d26041b25a21b
Rylande <frankmennink@gmail.com>
2020-12-22 12:37:43 +0100

    Shorthand

--- Day22.py
+++ Day22.py
@@ -11,10 +11,7 @@ def playCombat():
     deck1, deck2 = player1[:], player2[:]
     while len(deck1) > 0 and len(deck2) > 0:
         card1, card2 = deck1.pop(0), deck2.pop(0)
-        if card1 > card2:
-            deck1.extend([card1, card2])
-        else:
-            deck2.extend([card2, card1])
+        deck1.extend([card1, card2]) if card1 > card2 else deck2.extend([card2, card1])
     return scoreDeck(deck1) if len(deck1) > 0 else scoreDeck(deck2)
 
 def playRecursiveCombat(deck1, deck2):
@@ -26,16 +23,10 @@ def playRecursiveCombat(deck1, deck2):
         previousScores.add(tuple(deck2))
         card1, card2 = deck1.pop(0), deck2.pop(0)
         if len(deck1) < card1 or len(deck2) < card2:
-            if card1 > card2:
-                deck1.extend([card1, card2])
-            else:
-                deck2.extend([card2, card1])
+            deck1.extend([card1, card2]) if card1 > card2 else deck2.extend([card2, card1])
         else:
             score1, score2 = playRecursiveCombat(deck1[:card1], deck2[:card2])
-            if score1 > score2:
-                deck1.extend([card1, card2])
-            else:
-                deck2.extend([card2, card1])
+            deck1.extend([card1, card2]) if score1 > score2 else deck2.extend([card2, card1])
     return scoreDeck(deck1), scoreDeck(deck2)
 
 print(playCombat())
