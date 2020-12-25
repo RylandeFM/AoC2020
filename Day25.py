2909b03b22061d66b192c71db02bbe2348575cbc
Rylande <frankmennink@gmail.com>
2020-12-25 10:36:53 +0100

    Day 25

---
+++ Day25.py
@@ -0,0 +1,11 @@
+cardKey, doorKey = 3248366, 4738476
+
+def getEncryptionKey(publicKey, otherPublicKey):
+    loops, value, subject, modulo = 0, 1, 7, 20201227
+    while value != publicKey:
+        value = (value * subject) % modulo
+        loops += 1
+    return pow(otherPublicKey, loops, 20201227)
+
+print(getEncryptionKey(cardKey, doorKey))
+print(getEncryptionKey(doorKey, cardKey))
