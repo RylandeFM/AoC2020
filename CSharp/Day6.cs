7f489a952ef18b00de381b8472cec51209571a7c
Frank Mennink <fmennink@deloitte.nl>
2021-06-09 17:24:00 +0200

    Day 6

--- CSharp/Day6.cs
+++ CSharp/Day6.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.IO;
+using System.Collections.Generic;
 
 namespace CSharp
 {
@@ -14,12 +15,61 @@ namespace CSharp
 
         static void RunPart1(string[] inputData)
         {
-
+            HashSet<char> answers = new();
+            int totalCount = 0;
+            foreach (string line in inputData)
+            {
+                if (line == "")
+                {
+                    totalCount += answers.Count;
+                    answers = new();
+                }
+                else
+                {
+                    foreach (char letter in line)
+                    {
+                        answers.Add(letter);
+                    }
+                }
+            }
+            totalCount += answers.Count;
+            Console.WriteLine(totalCount);
         }
 
         static void RunPart2(string[] inputData)
         {
+            HashSet<char> answers, groupAnswers = new();
+            int totalCount = 0;
+            bool newGroup = true;
+            foreach (string line in inputData)
+            {
+                if (line == "")
+                {
+                    totalCount += groupAnswers.Count;
+                    groupAnswers = new();
+                    newGroup = true;
+                }
+                else
+                {
+                    answers = new();
+                    foreach (char letter in line)
+                    {
+                        if (newGroup)
+                        {
+                            groupAnswers.Add(letter);
+                        }
+                        else 
+                        {
+                            answers.Add(letter);
+                        }
+                    }
+                    if (!newGroup) { groupAnswers.IntersectWith(answers); }
+                    newGroup = false;
+                }
 
+            }
+            totalCount += groupAnswers.Count;
+            Console.WriteLine(totalCount);
         }
     }
 }
