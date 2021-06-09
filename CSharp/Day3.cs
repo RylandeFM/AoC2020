a01dc00a355cd7e19fa698a2f1a08ef30a47ddaa
Frank Mennink <fmennink@deloitte.nl>
2021-06-09 14:58:48 +0200

    Day3 solution

--- CSharp/Day3.cs
+++ CSharp/Day3.cs
@@ -14,12 +14,30 @@ namespace CSharp
 
         static void RunPart1(string[] inputData)
         {
-
+            int currentX = 0, noOfTrees = 0;
+            for (int i = 1; i < inputData.Length; i += 1)
+            {
+                currentX += 3;
+                noOfTrees += inputData[i][currentX % inputData[0].Length] == '#' ? 1 : 0;
+            }
+            Console.WriteLine(noOfTrees);
         }
 
         static void RunPart2(string[] inputData)
         {
+            Console.WriteLine(checkSlopes(inputData, 1, 1) * checkSlopes(inputData, 3, 1) * checkSlopes(inputData, 5, 1) * checkSlopes(inputData, 7, 1) * checkSlopes(inputData, 1, 2));
+        }
 
+        static long checkSlopes(string[] inputData, int stepX, int stepY)
+        {
+            int currentX = 0;
+            long noOfTrees = 0;
+            for (int i = stepY; i < inputData.Length; i += stepY)
+            {
+                currentX += stepX;
+                noOfTrees += inputData[i][currentX % inputData[0].Length] == '#' ? 1 : 0;
+            }
+            return noOfTrees;
         }
     }
 }
