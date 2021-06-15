27853eccba79513b11ae54b350897284257ca3e9
Frank Mennink <fmennink@deloitte.nl>
2021-06-15 16:56:38 +0200

    Day 8

--- CSharp/Day8.cs
+++ CSharp/Day8.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.IO;
+using System.Collections.Generic;
 
 namespace CSharp
 {
@@ -14,12 +15,86 @@ namespace CSharp
 
         static void RunPart1(string[] inputData)
         {
+            HandHeldConsole console = new();
+            HashSet<long> visitedIndexes = new();
 
+            while (!visitedIndexes.Contains(console.getProcessIndex()))
+            {
+                visitedIndexes.Add(console.getProcessIndex());
+                console.runCommand(inputData[console.getProcessIndex()]);
+            }
+            Console.WriteLine(console.getAccumulator());
         }
 
         static void RunPart2(string[] inputData)
         {
+            HashSet<long> swappedPositions = new();
+            HandHeldConsole console = new() ;
+            bool foundSwap = false;
 
+            while (!foundSwap) {
+                console = new();
+                HashSet<long> visitedIndexes = new();
+                bool hasSwapped = false;
+
+                while (!visitedIndexes.Contains(console.getProcessIndex()) && console.getProcessIndex() < inputData.Length)
+                {
+                    visitedIndexes.Add(console.getProcessIndex());
+                    if (inputData[console.getProcessIndex()].Split(" ")[0] != "acc" && !hasSwapped && !swappedPositions.Contains(console.getProcessIndex()))
+                    {
+                        string instruction = inputData[console.getProcessIndex()];
+                        instruction = instruction.Split(" ")[0] == "nop" ? instruction.Replace("nop", "jmp"): instruction.Replace("jmp", "nop");
+                        hasSwapped = true;
+                        swappedPositions.Add(console.getProcessIndex());
+                        console.runCommand(instruction);
+                    }
+                    else
+                    {
+                        console.runCommand(inputData[console.getProcessIndex()]);
+                    }
+                }
+
+                foundSwap = console.getProcessIndex() >= inputData.Length;
+            }
+            Console.WriteLine(console.getAccumulator());
         }
+    }
+
+    class HandHeldConsole
+    {
+        private long accumulator;
+        private long processIndex;
+
+        public HandHeldConsole()
+        {
+            accumulator = 0;
+            processIndex = 0;
+        }
+
+        public long getAccumulator() { return accumulator; }
+
+        private void adjustAccumulator(long amount)
+        {
+            accumulator += amount;
+        }
+
+        public void runCommand(string instruction)
+        {
+            switch (instruction.Split(" ")[0])
+            {
+                case "jmp":
+                    processIndex += long.Parse(instruction.Split(" ")[1]);
+                    break;
+                case "nop":
+                    processIndex += 1;
+                    break;
+                case "acc":
+                    adjustAccumulator(long.Parse(instruction.Split(" ")[1]));
+                    processIndex += 1;
+                    break;
+            }
+        }
+
+        public long getProcessIndex() { return processIndex; }
     }
 }
